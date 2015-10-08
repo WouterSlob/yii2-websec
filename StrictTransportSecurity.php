dd9c18e260d936505617a494ae8289bf9045d6d0
Wouter Slob <github@wslob.nl>
2015-10-08 16:51:22 +0200

    Create StrictTransportSecurity.php

---
+++ StrictTransportSecurity.php
@@ -0,0 +1,14 @@
+namespace wouterslob\yii2-websec;
+
+use yii\base\BootstrapInterface;
+use yii\base\Application;
+
+class StrictTransportSecurity implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+        $app->on(Application::EVENT_AFTER_REQUEST, function () {
+             // todo: modify headers
+        });
+    }
+}
